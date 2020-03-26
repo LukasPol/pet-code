07935556f1063f2f3e0e4d30f59a54d6a026fea1
Lukas Pol <lukaspaespol@gmail.com>
2020-03-26 17:34:11 -0300

    add functions

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,5 @@
 class AnimalsController < ApplicationController
   before_action :set_animal, only: [:show, :update, :destroy]
-  before_action :verify_total_cost, only: [:create]
 
   # GET /animals
   def index
@@ -17,8 +16,6 @@ class AnimalsController < ApplicationController
   # POST /animals
   def create
     @animal = Animal.new(animal_params)
-
-    @person = Person.find(@Animal.person_id)
 
     if @animal.save
       render json: @animal, status: :created, location: @animal
@@ -50,17 +47,5 @@ class AnimalsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def animal_params
       params.require(:animal).permit(:name, :monthlyCost, :type_animal_id, :person_id)
-    end
-
-    def verify_total_cost
-      @person_id = params[:animal][:person_id]
-      @animals_person = Animal.joins(:person).where(person_id: person_id)
-      @totalCost = 0 
-
-      @animals_person.each do |animal|
-        @totalCost += animal.monthlyCost
-      end
-
-      #### CONTINUE TEMP
     end
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,37 @@
 class Animal < ApplicationRecord
   belongs_to :type_animal
   belongs_to :person
+
+  before_create :verify_total_cost
+  before_create :verify_name_person
+
+  validates :name, presence: true
+
+  private
+    def verify_total_cost
+      @person_id = self.person_id
+      @animals_person = Animal.joins(:person).where(person_id: person_id)
+      @totalCost = 0 
+
+      @animals_person.each do |animal|
+        @totalCost += animal.monthlyCost
+      end
+
+      puts @totalCost
+
+      if @totalCost > 1000
+        self.errors.add(:monthlyCost, 'Curto mensal maior q 100')
+        throw(:abort)
+      end
+    end
+    def verify_name_person
+      @person_id = self.person_id
+      @personsName = Person.find(person_id).name
+      
+      if @personsName.first.upcase === 'A'
+        self.errors.add(:person, 'Pessoa com inical da letra A, não podem ter gatos')
+        throw(:abort)
+      end
+
+    end
 end
