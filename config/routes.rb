f947e13ce5745f7455e0e3288bc851ef49e1866c
Lukas Pol <lukaspaespol@gmail.com>
2021-02-21 18:50:44 -0300

    create user for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'api/v1/animals#index'
   namespace 'api' do
     namespace 'v1' do
+      mount_devise_token_auth_for 'User', at: 'auth',
+        controllers: { sessions: 'api/v1/sessions' }
       resources :animals
       resources :people
       resources :type_animals
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
